d09ba109d9b2984ecc57283ad78ef0d3ab8fdfaa
umut <umut_baykan@hotmail.com>
2023-07-31 23:21:42 +0100

    rerendering through recursive function

--- main.js
+++ main.js
@@ -18,3 +18,19 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff5e00,
+  wireframe: true,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
